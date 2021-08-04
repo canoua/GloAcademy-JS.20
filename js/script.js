a87e8b5f62a99e60ff0f5e8f363d3ab4b6685c75
akaryaka <a.karyaka.2dit@gmail.com>
2021-08-04 22:09:01 +0600

    feat(script.js): add function expression

--- js/script.js
+++ js/script.js
@@ -10,31 +10,16 @@ const money = prompt('Ваш месячный доход'),
 	mission = 15000 / budgetMonth,
 	budgetDay = budgetMonth / 30;
 
-
-console.log('Тип данных переменной ' + ' money - ' + typeof money + ','  + ' addExpenses - ' + typeof addExpenses + ','  + ' deposit - ' + typeof deposit + ','  + ' expenses1 - ' + typeof expenses1 + ','  + ' expenses2 - ' + typeof expenses2 + ','  + ' amount1 - ' + typeof amount1 + ','  + ' amount2 - ' + typeof amount2 + ','  + ' mission - ' + typeof mission);
-console.log(addExpenses.toLowerCase().split(', ')); 
-
-function getExpensesMonth(amount1, amount2) {
+const getExpensesMonth  = function(amount1, amount2) {
    return sum = Number(amount1) + Number(amount2);
 }
-console.log('1.Функция возвращает сумму всех обязательных расходов за месяц - ' + getExpensesMonth(amount1, amount2));
-
-function getAccumulatedMonth(money, sum) {
+const getAccumulatedMonth = function(money, sum) {
    return result = Number(money) - Number(sum);
 }
-console.log('2.Функция возвращает накопления за месяц (Доходы минус расходы) - ' + getAccumulatedMonth(money, sum)) ;
-
-let accumulatedMonth = getAccumulatedMonth(money, sum);
-
-function getTargetMonth(mission, accumulatedMonth) {
+const getTargetMonth = function(mission, accumulatedMonth) {
    return resultMission = Number(mission) / Number(accumulatedMonth);
 }
-
-console.log('За ' + Math.ceil(getTargetMonth(mission, accumulatedMonth)) + ' месяц(а) будет достигнута цель');
-
-console.log('Бюджет на день : ' + budgetDay);
-
-let getStatusIncome = function(budgetDay) {
+const getStatusIncome = function(budgetDay) {
    if (budgetDay >= 1200) {
       return ('У вас высокий уровень дохода');
    } else if (budgetDay >= 600 || budgetDay < 1200) {
@@ -46,4 +31,10 @@ let getStatusIncome = function(budgetDay) {
    }
 }
 
+console.log('Тип данных переменной ' + ' money - ' + typeof money + ','  + ' addExpenses - ' + typeof addExpenses + ','  + ' deposit - ' + typeof deposit + ','  + ' expenses1 - ' + typeof expenses1 + ','  + ' expenses2 - ' + typeof expenses2 + ','  + ' amount1 - ' + typeof amount1 + ','  + ' amount2 - ' + typeof amount2 + ','  + ' mission - ' + typeof mission);
+console.log(addExpenses.toLowerCase().split(', ')); 
+console.log('1.Функция возвращает сумму всех обязательных расходов за месяц - ' + getExpensesMonth(amount1, amount2));
+console.log('2.Функция возвращает накопления за месяц (Доходы минус расходы) - ' + getAccumulatedMonth(money, sum)) ;
+console.log('За ' + Math.ceil(getTargetMonth(mission, getAccumulatedMonth(money, sum))) + ' месяц(а) будет достигнута цель');
+console.log('Бюджет на день : ' + budgetDay);
 console.log(getStatusIncome(budgetDay));
